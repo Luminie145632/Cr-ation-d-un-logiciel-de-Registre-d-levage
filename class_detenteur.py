813be1b2bcaf69dacd5f0d14a11e1bea79c66429
Luminie145632 <john-killian.kennedy@edu.esiee-it.fr>
2024-01-22 16:00:49 +0100

    class_mouvement

--- class_detenteur.py
+++ class_detenteur.py
@@ -7,7 +7,7 @@ class Detenteur:
         self.Prenom_du_proprietaire = Prenom_du_proprietaire
         self.date_ouverture_elevage = date_ouverture_elevage
 
-        # Phrase qui donne le prénom et le nom du propriétaire du cheval.
+        # Phrase qui donne le prénom et le nom du propriétaire du cheval et qui donne la date d'ouverture du lieu ou se trouve le cheval.
         print(f"Le propriétaire de ce cheval s'appelle {self.Nom_du_proprietaire} {self.Prenom_du_proprietaire} et son élevage de chevaux a ouvert le {self.date_ouverture_elevage}.")
 
 # Exemple d'utilisation de la classe
