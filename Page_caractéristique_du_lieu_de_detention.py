bc137dc7b71ed8b8d0d1574a260606b0932db58c
Luminie145632 <john-killian.kennedy@edu.esiee-it.fr>
2024-01-23 10:11:25 +0100

    modif movement

---
+++ Page_caractéristique_du_lieu_de_detention.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+def create_window():
+    window = tk.Tk()
+    window.title("Fenêtre avec Boutons")
+
+    # Configuration de la taille de la fenêtre
+    window.geometry("1920x1080")
+
+    # Modification du logo de la fenêtre
+    window.iconbitmap(chemin_logo)
+
+    # Création du bouton Connexion
+    btn_connexion = tk.Button(window, text="Connexion", command=on_connexion_click)
+    btn_connexion.pack(pady=10)
+
+    # Création du bouton Création
+    btn_creation = tk.Button(window, text="Création", command=on_creation_click)
+    btn_creation.pack(pady=10)
+
+    # Exécution de la boucle principale
+    window.mainloop()
+
+# Appel de la fonction pour créer la fenêtre avec les boutons et le logo
+create_window()
