6a4e6eee34e9399aa7cfdd3768cf91a8ede15986
Luminie145632 <150667905+Luminie145632@users.noreply.github.com>
2024-02-07 15:57:47 +0100

    compte

--- view/Compte.py
+++ view/Compte.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
+from PIL import Image, ImageTk
+import glob
 
 class FenetrePrincipale(tk.Tk):
     def __init__(self):
@@ -17,20 +19,50 @@ class FenetrePrincipale(tk.Tk):
         self.canvas = tk.Canvas(self, width=1920, height=800)
         self.canvas.pack()
 
+        # Création du label pour afficher l'image
+        self.label_image = tk.Label(self.canvas)
+        self.label_image.pack(fill=tk.BOTH, expand=tk.YES)
+
+        # Appel à la méthode pour gérer le fond d'image changeant
+        self.setup_background_animation()
+
         # Création du bouton Création d'un compte
         btn_creation = ttk.Button(self, text="Création d'un compte", command=self.on_creation_click, style="TButton")
-        btn_creation.pack(pady=10, side=tk.TOP, anchor=tk.CENTER)
-        
+        self.canvas.create_window(950, 200, window=btn_creation, anchor='center')
+
         # Création du bouton Connexion
         btn_connexion = ttk.Button(self, text="Connexion", command=self.on_connexion_click, style="TButton")
-        btn_connexion.pack(pady=10, side=tk.TOP, anchor=tk.CENTER)
+        self.canvas.create_window(950, 250, window=btn_connexion, anchor='center')
 
-        
     def on_connexion_click(self):
         print("Bouton Connexion cliqué")
 
     def on_creation_click(self):
         print("Bouton Création d'un compte cliqué")
+
+    def setup_background_animation(self):
+        self.image_paths = glob.glob("/Creation_dun_logiciel_de_Registre_delevage/images/*.png")
+        self.current_image_index = 0
+        self.load_image()
+
+    def load_image(self):
+        image_path = self.image_paths[self.current_image_index]
+        image = Image.open(image_path)
+        image = image.resize((self.winfo_width(), self.winfo_height()))
+        photo = ImageTk.PhotoImage(image)
+        self.label_image.configure(image=photo)
+        self.label_image.image = photo
+        self.current_image_index = (self.current_image_index + 1) % len(self.image_paths)
+
+        self.after(2000, self.load_image)
+
+    def redimensionner_image(self, event):
+        image_path = self.image_paths[self.current_image_index]
+        image = Image.open(image_path)
+        image = image.resize((self.winfo_width(), self.winfo_height()))
+        photo = ImageTk.PhotoImage(image)
+        self.label_image.configure(image=photo)
+        self.label_image.image = photo
 
 if __name__ == "__main__":
     fenetre_principale = FenetrePrincipale()
